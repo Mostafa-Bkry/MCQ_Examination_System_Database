0a9bbcf24a16ed100ecd8eb422b235aec75db91d
Mostafa Bkry <mbkry661@gmail.com>
2024-06-18 20:52:18 +0300

    added courses table SP

--- Code/Stored_Procedures.sql
+++ Code/Stored_Procedures.sql
@@ -65,4 +65,72 @@ as
 		Select ERROR_NUMBER(), ERROR_MESSAGE(), ERROR_LINE()
 	end catch
 
+	
+-----------------------------------------------Courses-------------------------------------------------
+-------------------------------------------------------------------------------------------------------
 
+
+----------select all ------------ 
+create proc SelectAllCourses
+as
+begin
+	select * from Courses
+end
+
+----------select a Course ------------ 
+create proc SelectCourse @crs_id int
+as
+begin
+   select * from Courses where crs_id = @crs_id
+end
+
+-----------insert ------------ 
+create proc insertCourses @crsId int, @crsName varchar(50), 
+	@instId int, @dept_no int
+as
+	begin try
+		insert into Courses 
+		values (@crsId , @crsName , @instId, @dept_no)
+	end try
+	begin catch
+		Select 'Faild to insert into Courses'
+		Select ERROR_NUMBER(), ERROR_MESSAGE(), ERROR_LINE()
+	end catch
+
+
+----------update-----------
+create proc updateCourses @newCrsId int = null, @crsId int, 
+	@crsName varchar(50) = null, @instId int = null, @dept_no int = null
+as
+	begin try
+		Update Courses
+		set crs_id = (case when @newCrsId is not null then @newCrsId else @crsId end),
+			crs_name = (case when @crsName is not null then @crsName else crs_name end),
+			inst_id = (case when @instId is not null then @instId else inst_id end),
+			Dept_no = (case when @dept_no is not null then @dept_no else Dept_no end)
+		where crs_id = @crsId
+	end try
+	begin catch
+		Select 'Faild to update Course ' + crs_id
+		from Courses
+		where crs_id = @crsId
+		Select ERROR_NUMBER(), ERROR_MESSAGE(), ERROR_LINE()
+	end catch
+
+----------delete----------
+create proc DeleteCourses @crsId int 
+as
+	begin try
+		Delete from Courses  
+		where crs_id = @crsId
+	end try
+	begin catch
+		Select 'Faild to delete course ' + crs_id
+		from Courses
+		where crs_id = @crsId
+		Select ERROR_NUMBER(), ERROR_MESSAGE(), ERROR_LINE()
+	end catch
+
+
+
+----------------------------------------------------------------------------------------------------------------
